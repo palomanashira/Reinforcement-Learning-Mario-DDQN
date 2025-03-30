612c9cf478c0e4869bca321e8481eb77bbc2fa7f
palomanashira <115988037+palomanashira@users.noreply.github.com>
2025-03-30 18:27:17 +0100

    Update train_mario.py

--- train_mario.py
+++ train_mario.py
@@ -1,4 +1,4 @@
-import torch
+import torch, os
 import torch.nn as nn
 import numpy as np
 import gym_super_mario_bros
@@ -11,7 +11,7 @@ from tensordict import TensorDict
 from torchrl.data import TensorDictReplayBuffer, LazyMemmapStorage
 
 # Make environment
-env = gym_super_mario_bros.make('SuperMarioBros-1-1-v3', apply_api_compatibility=True)
+env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0', render_mode='rgb', apply_api_compatibility=True)
 # Restrict actions to right only
 env = JoypadSpace(env, RIGHT_ONLY)
 # Pre-process environment
@@ -22,7 +22,7 @@ env = FrameStack(env, num_stack=4)
 
 # define variables for training
 n_episodes = 100000
-n_save_iter = 500
+n_save_iter = 2
 lr = 25e-5
 batch_size = 64
 eps = 1
@@ -31,14 +31,25 @@ gamma = 0.9
 eps_min = 0.1
 replay_buffer_capacity = 100_000
 sync_network_rate = 200
+load_model = None #'trained_models/model_8_0.9994711398296666.pt'
+first_episode = 0
 
 # Choose and print device (mps for using GPU on MacOS, can use cuda if nvidia GPU is available)
-device = torch.device("mps:0") if torch.backends.mps.is_available() and torch.backends.mps.is_built() else 'cpu'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(device)
 
 # Instantiate model
-online_network = CNN(n_actions = env.action_space.n, n_frames = env.observation_space.shape[0]).to(device)
-target_network = CNN(n_actions = env.action_space.n, n_frames = env.observation_space.shape[0]).to(device)
+online_network = CNN(n_actions = env.action_space.n, 
+                         n_frames = env.observation_space.shape[0]).to(device)
+target_network = CNN(n_actions = env.action_space.n, 
+                         n_frames = env.observation_space.shape[0]).to(device)
+
+if not isinstance(load_model, type(None)):
+    online_network.load_state_dict(torch.load(load_model))
+    target_network.load_state_dict(online_network.state_dict())
+    base_name = os.path.basename(load_model)  # Extracts the file name from the path
+    first_episode = int(base_name.split('_')[1]) - 1 # Extracts last episode from file name
+    eps = float(base_name.split('_')[2].split('.')[0] + '.' + base_name.split('_')[2].split('.')[1]) # Extracts last epsilon from file name
 
 # Optimiser and loss
 optimizer = Adam(online_network.parameters(), lr= lr) # only pass online network parameters to optimizer
@@ -49,7 +60,7 @@ storage = LazyMemmapStorage(replay_buffer_capacity)
 replay_buffer = TensorDictReplayBuffer(storage=storage)
 
 # Training Loop
-for i in range(n_episodes):    
+for i in range(first_episode, n_episodes):    
     done = False
     state, _ = env.reset()
     total_reward = 0
@@ -69,7 +80,10 @@ for i in range(n_episodes):
         next_state, reward, done, truncated, info  = env.step(a)
         total_reward += reward
         # Store tuple in replay buffer
-        replay_buffer.add(TensorDict({"state": torch.tensor(np.array(state), dtype=torch.float32), "action": torch.tensor(a),"reward": torch.tensor(reward), "next_state": torch.tensor(np.array(next_state), dtype=torch.float32), "done": torch.tensor(done)}, batch_size=[]))
+        replay_buffer.add(TensorDict({"state": torch.tensor(np.array(state), dtype=torch.float32),
+                                      "action": torch.tensor(a),"reward": torch.tensor(reward), 
+                                      "next_state": torch.tensor(np.array(next_state), dtype=torch.float32), 
+                                      "done": torch.tensor(done)}, batch_size=[]))
         # Update state in currect episode
         state = next_state
 
@@ -84,7 +98,7 @@ for i in range(n_episodes):
             state_action_Qs = all_pred_Qs[np.arange(batch_size), actions.squeeze()]
             max_next_Qs = target_network(next_states).max(dim=1)[0]
             target_Qs = rewards + gamma * max_next_Qs * (1 - dones.float()) # "Grount Truth" provided by the target network put through action value function
-            # Calculate loss and undate network weights
+            # Calculate loss and update network weights
             loss = loss_fun(state_action_Qs, target_Qs)
             optimizer.zero_grad()
             loss.backward()
@@ -92,14 +106,18 @@ for i in range(n_episodes):
             # Decay epsilon
             eps = max(eps * eps_decay, eps_min)
 
-    print("Episode:", i, "Total reward:", total_reward, "Epsilon:", eps, "Size of replay buffer:", len(replay_buffer))
+    print("Episode:", i, 
+          "Total reward:", total_reward, 
+          "Epsilon:", eps, 
+          "Size of replay buffer:", len(replay_buffer))
 
     # Sync networks
     if i+1 % sync_network_rate == 0:
         target_network.load_state_dict(online_network.state_dict())
 
     #save model
-    if i+1 % n_save_iter == 0:
-        torch.save(online_network.state_dict(), 'trained_models/model_' + str(i+1) + '.pt')
+    if (i+1) % n_save_iter == 0:
+        online_network.save_model('/users/k20111668/workspace/mario/Mario/trained_models/model_' + str(i+1) + '_' + str(eps))
+        print('model_' + str(i+1) + '_' + str(eps), 'saved')
 
 env.close()
